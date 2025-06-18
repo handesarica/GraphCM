7c3cf7db906a7d66e28ecc429402563549f86155
Hande Sarica Kececi <sarica.hande@gmail.com>
2025-06-18 12:37:15 +0300

    Update model.py

--- model.py
+++ model.py
@@ -234,7 +234,7 @@ class Model(object):
     """
     def dcg(self, ranking_relevances):
         """
-        Computes the DCG for a given ranking_relevances
+        #Computes the DCG for a given ranking_relevances
         """
         return sum([(2 ** relevance - 1) / math.log(rank + 2, 2) for rank, relevance in enumerate(ranking_relevances)])
     """
